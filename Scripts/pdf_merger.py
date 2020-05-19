ded56b85adcf6ffa839f9f6954b03f7baae3be72
Andrei Draghiceanu <andrei.draghiceanu@gmail.com>
2020-05-20 00:26:09 +0300

    Updated Scripts

---
+++ Scripts/pdf_merger.py
@@ -0,0 +1,31 @@
+import PyPDF2
+import sys
+
+# with open('dummy.pdf', 'rb') as file:
+#     reader = PyPDF2.PdfFileReader(file)
+#     print(reader.numPages)
+#     print(reader.getPage(0))
+#     page = reader.getPage(0)
+#     page.rotateCounterClockwise(90)
+#     writer = PyPDF2.PdfFileWriter()
+#     writer.addPage(page)
+#     with open('tilt.pdf', 'wb') as new_file:
+#         writer.write(new_file)
+
+
+template = PyPDF2.PdfFileReader(open('super.pdf', 'rb'))
+watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+output = PyPDF2.PdfFileWriter()
+
+for i in range(template.getNumPages()):
+    page = template.getPage(i)
+    page.mergePage(watermark.getPage(0))
+    output.addPage(page)
+
+    with open('watermarked_output.pdf', 'wb') as file:
+        output.write(file)
+
+
+
+
+
